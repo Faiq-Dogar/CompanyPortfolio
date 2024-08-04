b9a2e28a072862529d8659f7300256edae72bd3a
myselfabdullah <myselfabdullah007@gmail.com>
2024-08-04 18:50:14 +0500

    SVGs Complete 4/Aug/24

--- frontend/src/pages/Services/Services.jsx
+++ frontend/src/pages/Services/Services.jsx
@@ -10,6 +10,11 @@ import ServiceCard from '../../Components/ServiceCard';
 import MobileDevelopemnt from '../../SVGs/MobileDevelopemnt'
 import WebDevelopemnt from '../../SVGs/WebDevelopemnt'
 import Ecommerce from '../../SVGs/Ecommerce'
+import UI from '../../SVGs/UI';
+import SEO from '../../SVGs/SEO';
+import UX from '../../SVGs/UX';
+import Flutter from '../../SVGs/Flutter';
+import SoftwareDeveleopment from '../../SVGs/SoftwareDeveleopment';
 
 const Services = ({ SERVICES_DATA, hoveredAvatar, handleMouseEnter, handleMouseLeave }) => {
     return (
@@ -69,20 +74,28 @@ const Services = ({ SERVICES_DATA, hoveredAvatar, handleMouseEnter, handleMouseL
                         </Grid>
                         <Grid item xs={12} md={4} lg={4}>
                             <ServiceCard
-                                heading={'Outsoursing Development'}
+                                heading={'Outsoursing Project Development'}
                                 text={'We help businesses elevate their value through custom software development, product design, QA and consultancy.'}
                                 item1={'User Interface'}
                                 item2={'Search Engine Optimization'}
                                 item3={'User Experience'}
+                                
+                                item1Svg={<UI/>}
+                                item2Svg={<SEO/>}
+                                item3Svg={<UX/>}
                             />
                         </Grid>
                         <Grid item xs={12} md={4} lg={4}>
                             <ServiceCard
                                 heading={'Software Product Development'}
                                 text={'We help businesses elevate their value through custom software development, product design, QA and consultancy.'}
-                                item1={'Desktop Applications'}
+                                item1={'Mobile Apps Development'}
                                 item2={'Software-As-A-Service'}
-                                item3={'Mobile Apps'}
+                                item3={'Desktop Applications'}
+
+                                item1Svg={<Flutter/>}
+                                item2Svg={<SEO/>}
+                                item3Svg={<SoftwareDeveleopment/>}
                             />
                         </Grid>
                     </Grid>
