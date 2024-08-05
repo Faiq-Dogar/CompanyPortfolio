6484fe59d552c8cad3609ff83fc2a8aab650bcc6
myselfabdullah <myselfabdullah007@gmail.com>
2024-08-05 19:12:24 +0500

    SingleServices layout 3/Aug/24

--- frontend/src/db/data.js
+++ frontend/src/db/data.js
@@ -58,17 +58,79 @@ const testimonialsList = [
         img: ''
     },
 ]
-
+const mobileApp1 = [
+    {
+        id: 1,
+        title: `Mobile App Development `,
+        para: `Zai Systems is one of the best companyies in Movile App Development.`,
+        para2: `At ArchTech, we pride ourselves on being one of the best leading mobile app development companies in Pakistan, delivering top-notch application development services that cater to businesses of all sizes.`,
+    }
+]
+const mobileApp2 = [
+    {
+      id: 1,
+      title: 'Android App Development Services',
+      para: `Our Android mobile app development company specializes in creating feature-rich, 
+      high-performance apps for the Android platform. Our developers are proficient in the latest 
+      technologies and follow best practices to ensure your app stands out in the crowded marketplace.`,
+    },
+    {
+      id: 2,
+      title: 'IOS App Development Services',
+      para: `As a mobile app development company for iOS, we develop elegant and intuitive apps that 
+      provide a seamless user experience. Our iOS development team is well-versed in Swift and Objective-C, 
+      ensuring your app is built to the highest standards.`,
+    },
+    {
+      id: 3,
+      title: 'Flutter App Development Services',
+      para: `As a mobile app development company for iOS, we develop elegant and intuitive apps that 
+      provide a seamless user experience. Our iOS development team is well-versed in Swift and Objective-C, 
+      ensuring your app is built to the highest standards.`,
+    },
+    {
+      id: 4,
+      title: 'Custom Mobile App Development',
+      para: `We offer custom mobile app development tailored to your specific business needs. Whether you need 
+      a simple app or a complex enterprise solution, our team has the expertise to deliver a product that meets your requirements.`,  
+    },
+    {
+      id: 5,
+      title: 'Mobile App Design Services',
+      para: `A great app starts with a great design. Our mobile app design services focus on creating intuitive 
+      and visually appealing interfaces that enhance user engagement and satisfaction.`,
+    },
+    {
+      id: 6,
+      title: 'Native Mobile App Development',
+      para: `Our native mobile app development services ensure that your app leverages the full potential of the 
+      device’s capabilities. We develop high-performance apps that provide a superior user experience.`,
+    },
+    {
+      id: 7,
+      title: 'Mobile App Prototyping',
+      para: `Before we start the development process, we create detailed prototypes to give you a clear vision of the final product. Our mobile app prototyping services help you make informed decisions and ensure that the end product aligns with your expectations.`,
+    }
+  ]
 
 export const Data = [
     {
-        id:1,
-        page:'/',
-        HomeTestimoials : testimonialsList
+        id: 1,
+        page: '/',
+        HomeTestimoials: testimonialsList
     },
     {
-        id:2,
-        page:'/Services',
-        HomeTestimoials : testimonialsList
+        id: 2,
+        page: '/Services',
+        HomeTestimoials: testimonialsList
+    }
+]
+export const SingleServiceData = [
+    {
+        id: 1,
+        service: 'Mobile App Developemnt',
+        HeroSectionData: mobileApp1,
+        MarqueeData: 'ArchTech Industries',
+        SingleServicelist: mobileApp2
     }
 ]
